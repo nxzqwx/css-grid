82a12b7adeaba7fa1e01f32c42fa066d7cecad06
nxzqwx <gredatoni@gmail.com>
2018-05-29 11:08:27 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>xxx</title>
+<link rel="stylesheet" type="text/css" href="all.css">
+</head>
+
+<body>
+
+<container>
+  <div class="title">
+    <h1>Ovo je naš naslov</h1>
+  </div>
+  <div class="share">SHARE</div>
+  <picture class="coverimage">
+    <img src="https://picsum.photos/300/200/?random"></picture>
+  <div class="postcontent">
+    <p>Lorem Ipsum je jednostavno probni tekst koji se koristi u tiskarskoj i slovoslagarskoj industriji. Lorem Ipsum postoji kao industrijski standard još od 16-og stoljeća, kada je nepoznati tiskar uzeo tiskarsku galiju slova i posložio ih da bi napravio
+      knjigu s uzorkom tiska. Taj je tekst ne samo preživio.</p>
+    <p>Lorem Ipsum je jednostavno probni tekst koji se koristi u tiskarskoj i slovoslagarskoj industriji. Lorem Ipsum postoji kao industrijski standard još od 16-og stoljeća, kada je nepoznati tiskar uzeo tiskarsku galiju slova i posložio ih da bi napravio
+      knjigu s uzorkom tiska. Taj je tekst ne samo preživio.</p>
+  </div>
+  <div class="form">
+    <form action="/action_page.php">
+      First name:<br>
+      <input type="text" name="firstname" value="Mickey">
+      <br> Last name:<br>
+      <input type="text" name="lastname" value="Mouse">
+      <br><br>
+      <input type="submit" value="Submit">
+    </form>
+  </div>
+
+
+
+</container>
+
+<div class="info">Resize this panel to see grids respond</div>
+</body>
+
+</html>
